455b25ac38fcb0f317616dab892f31c10d77643d
satyarevathi35 <satyaists3513@gmail.com>
2025-07-02 18:19:39 +0530

    Add files via upload

---
+++ nested.py
@@ -0,0 +1,11 @@
+#A Program on weather advisories
+is_snowing=True
+temp=int(input("enter the temperature.."))
+if is_snowing>10:
+    print("please carry umbrella..")
+    if is_snowing==-10:
+        print("please carry umbrella and jacket")
+    else:
+        print("enjoy the sunny day")
+else:
+          print("u have a great day")
